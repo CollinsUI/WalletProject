2d909a543b017cf73d6d393da1d0ed017cce027c
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2023-03-13 12:52:49 +0300

    Initial Commit

---
+++ WalletProject/WalletProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletProjectApp.swift
+//  WalletProject
+//
+//  Created by Collins Muthomi on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
